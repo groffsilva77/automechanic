59a6332f3c5b0d6f62389754961a8f426aa61d38
unknown <bielgroff2004@gmail.com>
2026-06-15 19:23:52 -0300

    first commit

---
+++ mechanic/src/main/java/mechanic/App.java
@@ -0,0 +1,13 @@
+package mechanic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
